b434171ba2878c2af3831941c9154967f22780b7
Axel Rindle <mail@axelrindle.de>
2023-01-30 14:51:50 +0100

    Use mutations

--- packages/frontend/src/modals/CreateRoom.tsx
+++ packages/frontend/src/modals/CreateRoom.tsx
@@ -1,64 +1,64 @@
-import { faCheck, faClose, faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { useCallback, useState } from 'react';
-import Input from '../components/form/Input';
-import Modal, { ChildProps } from '../components/Modal';
-import { useDispatch, useSelector } from '../store';
-import { clearFormData } from '../store/slices/formData'
+import { faCheck, faClose, faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { useState } from 'react'
+import Input from '../components/form/Input'
+import Modal, { ChildProps } from '../components/Modal'
+import { useDispatch, useSelector } from '../store'
+import { clearFormData, FormData } from '../store/slices/formData'
 
 const FORMDATA_KEY = 'room_create'
 
 interface Props extends ChildProps {}
 
-async function sendRequest(api: string, data: Record<string, any>): Promise<void> {
-    const response = await fetch(new URL('/api/room', api), {
-        body: JSON.stringify(data),
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    })
-    if (!response.ok || response.status !== 200) {
-        return Promise.reject('Error creating a Room! ' + response.statusText)
-    }
-}
-
 export default function ModalCreateRoom(props: Props) {
-    const [error, setError] = useState<string|null>(null)
     const [showPassword, setShowPassword] = useState(false)
     const formData = useSelector(state => state.formData[FORMDATA_KEY])
     const apiUrl = useSelector(state => state.config.apiUrl)
     const dispatch = useDispatch()
-
-    const { close } = props
-    const onSave = useCallback(async () => {
-        try {
-            setError(null)
-            await sendRequest(apiUrl, formData)
-            close()
+    const queryClient = useQueryClient()
+    const mutation = useMutation<Response, any[], FormData>({
+        mutationFn: data => fetch(new URL('/api/room', apiUrl), {
+            body: JSON.stringify(data),
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }),
+        onSuccess: () => {
+            queryClient.invalidateQueries({
+                queryKey: ['rooms']
+            })
+            props.close()
             dispatch(clearFormData(FORMDATA_KEY))
-        } catch (error: any) {
-            setError(error.message ?? error ?? 'Unknown error!')
         }
-    }, [apiUrl, formData, close, dispatch])
+    })
+
+    function getError(key: string): string | undefined {
+        if (!mutation.error) return undefined
+        if (!Array.isArray(mutation.error)) return undefined
+
+        const detail = mutation.error.find(el => el.context.key === key)
+        return detail.message
+    }
 
     return (
         <Modal
             isOpen={props.isOpen}
             disableClose={props.disableClose}
-            close={() => close()}
+            close={() => props.close()}
             title="New Room"
             subtitle="Create a new Room"
             actions={[
                 {
                     label: 'Cancel',
                     icon: faClose,
-                    handle: () => close(),
+                    handle: () => props.close(),
                 },
                 {
                     label: 'Create',
                     icon: faCheck,
-                    handle: () => onSave(),
+                    handle: () => mutation.mutate(formData),
                 },
             ]}
         >
@@ -69,6 +69,7 @@ export default function ModalCreateRoom(props: Props) {
                     label="Name"
                     placeholder="My awesome room"
                     formData={FORMDATA_KEY}
+                    error={getError('name')}
                 />
 
                 <Input
@@ -100,11 +101,6 @@ export default function ModalCreateRoom(props: Props) {
                     containerClassName="col-span-2"
                 />
             </div>
-            {error && (
-                <p className="text-red-500">
-                    {error}
-                </p>
-            )}
         </Modal>
     )
 }
