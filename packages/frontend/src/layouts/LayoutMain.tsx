86084ea2cbf90463945800782c8e06b734e00c7e
Axel Rindle <mail@axelrindle.de>
2023-01-31 08:52:19 +0100

    Add basic footer

--- packages/frontend/src/layouts/LayoutMain.tsx
+++ packages/frontend/src/layouts/LayoutMain.tsx
@@ -1,6 +1,25 @@
+import { faHeart } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { ReactNode } from 'react'
 import { Outlet } from 'react-router-dom'
 import Content from '../components/Content'
 import Navbar from '../components/navbar'
+
+interface LinkProps {
+    href: string
+    children: ReactNode
+}
+
+function Link(props: LinkProps) {
+    return (
+        <a
+            href={props.href}
+            className="underline"
+        >
+            {props.children}
+        </a>
+    )
+}
 
 export default function LayoutMain() {
     return (
@@ -9,14 +28,22 @@ export default function LayoutMain() {
 
             <div
                 className="
-                    mb-8 mt-12
-                    container max-w-screen-xl mx-auto
-                    flex flex-col
+                    container max-w-screen-xl
+                    mx-auto py-12
+                    flex-1 flex flex-col
                 "
             >
                 <Content border>
                     <Outlet />
                 </Content>
+            </div>
+
+            <div className="w-full h-16 bg-primary text-white drop-shadow-md">
+                <div className="container max-w-screen-xl h-full mx-auto flex items-center justify-center">
+                    <p>
+                        <Link href="https://github.com/axelrindle/planningpoker">Planning Poker</Link> made with <FontAwesomeIcon icon={faHeart} /> by <Link href="https://github.com/axelrindle">Axel Rindle</Link>
+                    </p>
+                </div>
             </div>
         </>
     )
